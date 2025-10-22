0af6285a8d8571fbe0efcf0f8ef8c5b6b145021e
Aline Carolina <eualine2017@gmail.com>
2025-10-22 15:31:00 -0300

    :white_check_mark:fix: Atualizando headers da requisição a API com key funcionando

--- backend/src/services/movieService.js
+++ backend/src/services/movieService.js
@@ -4,9 +4,12 @@ import { TMDB_BASE_URL, TMDB_API_KEY } from "../config/tmdbConfig.js";
 export async function getMoviesFromTMDB(query) {
   const response = await axios.get(`${TMDB_BASE_URL}/search/movie`, {
     params: {
-      api_key: TMDB_API_KEY,
       query: query,
     },
+    headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${process.env.TMDB_API_KEY}`
+  }
   });
   return response.data.results;
 }
